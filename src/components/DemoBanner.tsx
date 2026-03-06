0a593753031dcb3c3022f3df278c0587e79bc9b9
Raahiltfb <raahil.mehta003@gmail.com>
2026-03-06 14:24:17 +0530

    Update DemoBanner.tsx

--- src/components/DemoBanner.tsx
+++ src/components/DemoBanner.tsx
@@ -4,7 +4,7 @@ import { CLINIC_DATA } from '../constants';
 export const DemoBanner = () => {
   return (
     <div className="bg-white text-black/70 py-2 px-6 text-center text-[10px] uppercase tracking-[0.2em] font-bold">
-      Sample website concept prepared for <span className="text-white">{CLINIC_DATA.name}</span>
+      Sample website concept prepared for <span className="text-black">{CLINIC_DATA.name}</span>
     </div>
   );
 };
